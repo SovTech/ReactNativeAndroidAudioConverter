73ec486e4219287d90660ab0de6141d0e5bbc699
Jonathan Irwin <jonoiriwnrsa@gmail.com>
2017-04-17 22:57:22 +0200

    Feature: got audio converter working on the android side - no bridge created yet

--- android/src/main/java/za/co/sovtech/rnaac/ReactNativeAndroidAudioConverterModule.java
+++ android/src/main/java/za/co/sovtech/rnaac/ReactNativeAndroidAudioConverterModule.java
@@ -1,16 +1,37 @@
 package za.co.sovtech.rnaac;
 
-import android.app.Activity;
+import android.os.Environment;
+import android.util.Log;
 
-import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.ReadableMap;
+
+import java.io.File;
+
+import cafe.adriel.androidaudioconverter.AndroidAudioConverter;
+import cafe.adriel.androidaudioconverter.callback.IConvertCallback;
+import cafe.adriel.androidaudioconverter.callback.ILoadCallback;
+import cafe.adriel.androidaudioconverter.model.AudioFormat;
 
 public class ReactNativeAndroidAudioConverterModule extends ReactContextBaseJavaModule {
-    public ReactNativeAndroidAudioConverterModule(ReactApplicationContext reactContext) {
+    public ReactNativeAndroidAudioConverterModule(final ReactApplicationContext reactContext) {
         super(reactContext);
+        AndroidAudioConverter.load(reactContext, new ILoadCallback() {
+            @Override
+            public void onSuccess() {
+                Log.d("jono message", "success");
+                convertAudioFile(reactContext, "my_audio.flac");
+            }
+
+            @Override
+            public void onFailure(Exception error) {
+                Log.d("jono message", "failure");
+                Log.d("jono message", error.toString());
+                error.printStackTrace();
+                // FFmpeg is not supported by device
+            }
+        });
     }
 
     @Override
@@ -19,11 +40,35 @@ public class ReactNativeAndroidAudioConverterModule extends ReactContextBaseJava
     }
 
     @ReactMethod
-    public void openSelectDialog(ReadableMap config, Callback successCallback, Callback cancelCallback) {
-        Activity currentActivity = getCurrentActivity();
+    public void convertAudioFile(ReactApplicationContext reactContext, String fileName) {
+        File flacFile = new File(Environment.getExternalStorageDirectory(), fileName);
+        Log.d("jono message", Environment.getExternalStorageDirectory().toString());
+        IConvertCallback callback = new IConvertCallback() {
+            @Override
+            public void onSuccess(File convertedFile) {
+                // So fast? Love it!
+                Log.d("jono message", "convert success");
+            }
 
-        if (currentActivity == null) {
-            cancelCallback.invoke("Activity doesn't exist");
-        }
+            @Override
+            public void onFailure(Exception error) {
+                // Oops! Something went wrong
+                Log.d("jono message", "convert failure");
+                Log.d("jono message", error.toString());
+                error.printStackTrace();
+            }
+        };
+        AndroidAudioConverter.with(reactContext)
+                // Your current audio file
+                .setFile(flacFile)
+
+                // Your desired audio format
+                .setFormat(AudioFormat.MP3)
+
+                // An callback to know when conversion is finished
+                .setCallback(callback)
+
+                // Start conversion
+                .convert();
     }
 }
