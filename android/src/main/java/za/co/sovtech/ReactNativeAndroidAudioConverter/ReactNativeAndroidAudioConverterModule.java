c3c3b4eab2ef82a3da225244b6802636ca9ea32a
Jonathan Irwin <jonoiriwnrsa@gmail.com>
2017-04-17 21:00:41 +0200

    Feature: created basic structure of android package

---
+++ android/src/main/java/za/co/sovtech/ReactNativeAndroidAudioConverter/ReactNativeAndroidAudioConverterModule.java
@@ -0,0 +1,29 @@
+package za.co.sovtech.ReactNativeAndroidAudioConverter;
+
+import android.app.Activity;
+
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+
+public class ReactNativeAndroidAudioConverterModule extends ReactContextBaseJavaModule {
+    public ReactNativeAndroidAudioConverterModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return "ReactNativeAndroidAudioConverter";
+    }
+
+    @ReactMethod
+    public void openSelectDialog(ReadableMap config, Callback successCallback, Callback cancelCallback) {
+        Activity currentActivity = getCurrentActivity();
+
+        if (currentActivity == null) {
+            cancelCallback.invoke("Activity doesn't exist");
+        }
+    }
+}
