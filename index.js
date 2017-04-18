044ae22367ee925fb0f678dc9e8649323ccb847c
Jonathan Irwin <jonoiriwnrsa@gmail.com>
2017-04-18 14:28:51 +0200

    Feature: added function prototypes to JS wrapper

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ import {NativeModules} from 'react-native';
 
 let AudioConverter = NativeModules.ReactNativeAndroidAudioConverter;
 
+AudioConverter.prototype.convertAudioFile = function (filename, sucCallback, errCallback) {
+    AudioConverter.convertAudioFile(filename, (data) => sucCallback(data), (error) => errCallback(error))
+};
+AudioConverter.prototype.getFilePath = function () {
+    AudioConverter.getFilePath()
+};
+
 module.exports = AudioConverter;
