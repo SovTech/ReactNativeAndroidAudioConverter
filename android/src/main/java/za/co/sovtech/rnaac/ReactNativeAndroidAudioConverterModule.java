e62fd28e4ad4183cfd51581bd10e706b0465b48b
Jonathan Irwin <jonoiriwnrsa@gmail.com>
2017-04-18 13:55:16 +0200

    Feature: added function to return the file path of the input and output files Feature: got the convert function to work and to take the input file as a parameter - returns a promise on success/fail

--- android/src/main/java/za/co/sovtech/rnaac/ReactNativeAndroidAudioConverterModule.java
+++ android/src/main/java/za/co/sovtech/rnaac/ReactNativeAndroidAudioConverterModule.java
@@ -3,9 +3,12 @@ package za.co.sovtech.rnaac;
 import android.os.Environment;
 import android.util.Log;
 
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.uimanager.IllegalViewOperationException;
 
 import java.io.File;
 
@@ -20,45 +23,74 @@ public class ReactNativeAndroidAudioConverterModule extends ReactContextBaseJava
         AndroidAudioConverter.load(reactContext, new ILoadCallback() {
             @Override
             public void onSuccess() {
-                Log.d("jono message", "success");
-                convertAudioFile(reactContext, "my_audio.flac");
+                Log.d("rnaac notice", "successfully loaded AndroidAudioConverter");
             }
 
             @Override
             public void onFailure(Exception error) {
-                Log.d("jono message", "failure");
-                Log.d("jono message", error.toString());
-                error.printStackTrace();
                 // FFmpeg is not supported by device
+                Log.d("rnaac notice", error.toString());
+                error.printStackTrace();
             }
         });
     }
 
+    /**
+     * Function to return the name of the native module
+     *
+     * @return - native module name
+     */
     @Override
     public String getName() {
         return "ReactNativeAndroidAudioConverter";
     }
 
+    /**
+     * Function to return the file path of the folder where the converter will look for files
+     *
+     * @param promise - promise to return file path
+     */
     @ReactMethod
-    public void convertAudioFile(ReactApplicationContext reactContext, String fileName) {
+    public void getFilePath(Promise promise) {
+        try {
+            promise.resolve(Environment.getExternalStorageDirectory().toString());
+        } catch (IllegalViewOperationException e) {
+            promise.reject(e);
+        }
+    }
+
+    /**
+     * Function to convert a given audio file to MP3 format
+     *
+     * @param fileName        - input file name
+     * @param errorCallback   - function called if the file could not be converted
+     * @param successCallback - function called if the conversion was successful
+     */
+    @ReactMethod
+    public void convertAudioFile(String fileName,
+                                 final Callback errorCallback,
+                                 final Callback successCallback) {
         File flacFile = new File(Environment.getExternalStorageDirectory(), fileName);
-        Log.d("jono message", Environment.getExternalStorageDirectory().toString());
+        Log.d("Output file location", Environment.getExternalStorageDirectory().toString() + fileName);
+
+
         IConvertCallback callback = new IConvertCallback() {
             @Override
             public void onSuccess(File convertedFile) {
                 // So fast? Love it!
-                Log.d("jono message", "convert success");
+                Log.d("rnaac notice", "convert success");
+                successCallback.invoke("convert success");
             }
 
             @Override
             public void onFailure(Exception error) {
                 // Oops! Something went wrong
-                Log.d("jono message", "convert failure");
-                Log.d("jono message", error.toString());
+                errorCallback.invoke("convert failure: " + error.toString());
+                Log.d("rnaac notice", error.toString());
                 error.printStackTrace();
             }
         };
-        AndroidAudioConverter.with(reactContext)
+        AndroidAudioConverter.with(this.getReactApplicationContext())
                 // Your current audio file
                 .setFile(flacFile)
 
